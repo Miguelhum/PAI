070c246b0097888497d4ec101cde0b387fea3cf7
Miguelhum <miguelperezherasme@gmail.com>
2024-10-11 18:37:18 +0200

    hola

---
+++ l.js
@@ -0,0 +1,4 @@
+let a = 25;
+let b = 15 + a;
+let c = b * 2;
+console.log(a,b,c); 
--- preguntas.js
+++ preguntas.js
@@ -1,46 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Noms de variables
-1.1.- Digues quins d'aquests noms de variables són correctes i quins no.
-1. variAble si
-2. Variable si
-3. vari-able no
-4. vari_able si
-5. _variable si
-6. vari.able no
-7. vari4able si
-8. 4variable no
-9. variable4 si
-10.variable_4 si
-11.LaMevaVariable si
+//2.1.- Què passaria si féssim aquestes declaracions de variables?
+// Pensa primer que creus que passaria. Després pots provar a executar aquestes instruccions
+// i veure que passa (si passa alguna cosa).
 
-1.2.- Volem guardar a una variable el nom d'un empleat. Quin d'aquests noms de variable
-seria el més addient?
-1. nomempleat
-2. Nomempleat 
-3. NomEmpleat 
-4. nomEmpleat esta
-5. Nom_empleat
-6. _NomEmpleat
-7. nomEmpleat0
-8. nom_empleat esta
-9. nom0empleat
-10.Nom empleat
-
-2.1.- Què passaria si féssim aquestes declaracions de variables?
-Pensa primer que creus que passaria. Després pots provar a executar aquestes instruccions
-i veure que passa (si passa alguna cosa).
-a.-
 var a = 1;
 var a = 2;
-console.log(a); da 2
-b.-
+console.log(a); // da 2
+// b.-
 var a = 1;
 let a = 2;
 console.log(a);Error
@@ -53,9 +18,8 @@ let a = 1;
 let a = 2;
 console.log(a); Error
 
-3.1.- Quines d'aquestes assignacions són correctes i quines no? Tracta cada assignació
-com si fos independent.
-let a = 5; SI
+//3.1.- Quines d'aquestes assignacions són correctes i quines no? Tracta cada assignació si fos independent.
+let a = 5; //SI
 let b = 5z;
 let c = "c"; SI
 let c = "c';
@@ -64,7 +28,7 @@ let c = "c'c"c';
 let n = 35; SI
 let n = 35va;
 
-3.2.- Tenim un programa que té les assignacions que tenim a continuació. Hi ha, però, una
+//3.2.- Tenim un programa que té les assignacions que tenim a continuació. Hi ha, però, una
 línia errònia. Quina és? Escriu en un paper quina creus que és i desprès executa el codi.
 Fixa't en l'error que et dona el navegador.
 let a = 5;
@@ -73,21 +37,21 @@ c = 7;
 c no está definida. la línia incorrecta es la 2.
 
 
-3.3.- De quins tipus són les dades que assignem a aquestes variables?
+//3.3.- De quins tipus són les dades que assignem a aquestes variables?
 let a = 55; Numéricas
 let b = 55.5; Numéricas
 let c = "Hola"; string
 let d = 'Hola'; string
 let e = true; Lógicas
 
-4.1.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+//4.1.- Quins valors s'escriuran a la consola després d'executar aquest codi?
 let a = 25;
 let b = 15 + a;
 let c = b * 2;
 console.log(a,b,c); 
 R/: 25,40,80
 
-4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+//4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
 let a = 10;
 let b = 20;
 let c = 5;
@@ -100,7 +64,7 @@ c = c + 3 - b + 2;//30
 console.log(a,b,c);
 R/: 42,4,30
 
-4.3.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+//4.3.- Quins valors s'escriuran a la consola després d'executar aquest codi?
 Escriu-los en un paper i desprès executa el codi. Et donen el mateix?
 let a = 5;
 let b = 18;
@@ -117,7 +81,7 @@ d = b + b;
 console.log (a,b,c,d)
 R/: 16,36,64,72.
 
-4.4.- Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer
+//4.4.- Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer
 sobre paper i desprès prova a l’ordinador.
 let a = 10
 let b = 20
@@ -138,7 +102,7 @@ console.log(b);
 c = c + 3 - b + 2
 console.log(c);
 
-4.5. - Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer
+//4.5. - Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer
 sobre paper i desprès prova a l’ordinador.
 let a = 5;
 let b = 18;
@@ -155,7 +119,7 @@ c = b + c;
 d = b + b;
 console.log(a,b,c,d);
 
-4.6. - Quins valors s’escriuran a la consola si executem aquest programa?
+//4.6. - Quins valors s’escriuran a la consola si executem aquest programa?
 let a = 10;
 let b = 5;
 let c = a / b;
@@ -163,14 +127,14 @@ let d = a * b;
 let e = a % 3;
 console.log (c,d,e);
 
-4.7.- Quins valors s’escriuran a la consola si executem aquest programa?
+//4.7.- Quins valors s’escriuran a la consola si executem aquest programa?
 let a = 10;
 let b = 5;
 let c = a * b * 3;
 let d = a / 3;
 console.log(c,d);
 
-5.1.- Què s’escriurà a la consola si executem aquest codi?
+//5.1.- Què s’escriurà a la consola si executem aquest codi?
 let a = 5;
 a++;
 console.log(a);//6
@@ -178,7 +142,7 @@ let b = a++;
 console.log(a,b);
 R/:6,6
 
-5.2.- Què s’escriurà a la consola si executem aquest codi?
+//5.2.- Què s’escriurà a la consola si executem aquest codi?
 let a = 5;
 ++a;
 console.log(a);
@@ -186,8 +150,8 @@ let b = ++a;
 console.log(a,b); 
 R/6,6
 
-<p>.3.- Observa aquestes operacions, prova a executar-les i explica el perquè dels resultats
-obtinguts.</p>
+//5.3.- Observa aquestes operacions, prova a executar-les i explica el perquè dels resultats
+obtinguts.
 let a = 5;
 let b = a++;
 let c = a--;
@@ -207,7 +171,7 @@ let e = 25 + ++a;
 console.log(d,e);
 R/:31,31
 
-<p>.1.- Què s’escriurà a la consola si executes aquest codi?</p>
+//6.1.- Què s’escriurà a la consola si executes aquest codi?
 let a = 10;
 a += 5;
 console.log(a);//true
@@ -218,20 +182,16 @@ console.log(a);/false
 a /= 5;
 console.log(a);/error
 
-7.1.- Quines d’aquestes assignacions de cadenes de caràcters són incorrectes?
+//7.1.- Quines d’aquestes assignacions de cadenes de caràcters són incorrectes?
 let a = "Hola"; correcto
 let b = 'Hola'; correcto
 let c = 'Hola"; incorrecto
 let d = "Hola 'gramola'";correcto
 let e = 'Hola "gramola'";incorrecto
 
-7.2.- Què s’escriurà a la consola si executes aquest codi?
+//7.2.- Què s’escriurà a la consola si executes aquest codi?
 let a = "Hola";
 let b = "Adéu";
 console.log (a+b);//holaadéu.
 let c = a + 2;
 console.log (c);Hola2.
-
-<script src="preguntas.js"></script>
-</body> 
-</html>
